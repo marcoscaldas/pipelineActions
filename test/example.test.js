a7c4905a04ef5e5a1f65f4f0cf7dafa875263d2f
MarcosProfAds <seu-email@exemplo.com>
2024-11-07 23:10:07 -0300

    first commit

---
+++ test/example.test.js
@@ -0,0 +1,8 @@
+// test/example.test.js
+const assert = require('assert');
+
+describe('Exemplo de Teste', function() {
+  it('deve somar dois números corretamente', function() {
+    assert.strictEqual(1 + 12, 13);
+  });
+});
